379cb44f85e6acf734ca7d9476a1fcad3c69bbfc
uPiscium <upiscium@gmail.com>
2025-03-02 10:37:51 +0900

    Support flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.pkg-config
+          pkgs.mdbook
+        ];
+      };
+    };
+}
+
